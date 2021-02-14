1559b179432ced5afc27078b7cc6ec5cd0090c15
yidaveding <yi.dave.ding@gmail.com>
2021-02-15 02:10:38 +1100

    David week 2

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -2,11 +2,24 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+import seaborn as sns
 from sklearn.metrics import roc_curve, roc_auc_score
 from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score, balanced_accuracy_score
+from imblearn.over_sampling import SMOTE
 
 def plot_roc_curve(pred, ydata):
-    
+    """plot the roc curve
+
+    Parameters
+    ----------
+    pred: numpy array
+        predicted
+    ydata: numpy array
+        scaler to be used to scale data
+
+    Returns
+    -------
+    """
     # plot the roc curve
     #
     # input:
@@ -40,15 +53,40 @@ def plot_roc_curve(pred, ydata):
     plt.show()
     
 def eval_metrics(y_pred, y_test, praba_threshold=0.5):
+    """plot the roc curve
+
+    Parameters
+    ----------
+    pred: numpy array
+        predicted
+    ydata: numpy array
+        scaler to be used to scale data
+
+    Returns
+    -------
+    """
+    
+    # convert to binary from probability score
     predx = [x for x in y_pred > praba_threshold]
-    cm = confusion_matrix(y_test, predx).ravel()
-    print(f'ROC_AUC Score: {round(roc_auc_score(y_test, y_pred),3)}')
+    
+    # create confusion matrix
+    cf_matrix = confusion_matrix(y_test, predx)
+    
     print(f'-----------\nConfusion Matrix with thresthold of {praba_threshold}:')
-    print(f'TN: {cm[0]}, FP: {cm[1]}\nFN: {cm[2]}, TP: {cm[3]}')
+
+    # plot confusion matrix
+    group_names = ['True Neg','False Pos','False Neg','True Pos']
+    group_counts = ["{0:0.0f}".format(value) for value in cf_matrix.flatten()]
+    group_percentages = ['{0:.2%}'.format(value) for value in cf_matrix.flatten()/np.sum(cf_matrix)]
+    labels = [f"{v1}\n{v2}\n{v3}" for v1, v2, v3 in zip(group_names,group_counts,group_percentages)]
+    labels = np.asarray(labels).reshape(2,2)
+    sns.heatmap(cf_matrix, annot=labels, fmt='', cmap='Blues')
+    plt.show()
+    
+    # print evaluation scores
     print(f'{np.sum(predx)} out of {len(predx)} or {int(round(np.sum(predx)/len(predx)*100,0))}% of players are predicted to be 5+ Yrs')
     print(f'-----------\nRecall: {round(recall_score(y_test, predx), 4)}')
     print(f'Accuracy: {round(accuracy_score(y_test, predx), 4)}')
-    print(f'Balanced Accuracy: {round(balanced_accuracy_score(y_test, predx), 4)}')
     print(f'F1: {round(f1_score(y_test, predx), 4)}')
     print(f'-----------')
     plot_roc_curve(y_pred, y_test)
